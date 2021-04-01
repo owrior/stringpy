5750419e4da5621315584527f23f9c989550cd97
Georgia Tickle <georgiatickle@georgias-air.lan>
2021-04-01 18:38:44 +0100

    Finished and tested str_replace_all.

--- src/stringpy/str_replace.py
+++ src/stringpy/str_replace.py
@@ -20,15 +20,28 @@ def _replace(string, pattern, _pattern, replace, count):
         raise ValueError("Please enter either a string or dict for pattern.")
     return string
 
-def str_replace(string, pattern, replace):
+def str_replace(string, pattern, replace, count=1):
     if isinstance(string, _pd.Series):
         _pattern = _compile(pattern)
         def _pd_rep(x):
-            return _replace(x, pattern, _pattern, replace, 1)
+            return _replace(x, pattern, _pattern, replace, count)
         return string.apply(_pd_rep)
     elif isinstance(string, str):
         _pattern = _compile(pattern)
-        string = _replace(string, pattern, _pattern, replace, 1)
+        string = _replace(string, pattern, _pattern, replace, count)
+        return string
+    else:
+        raise ValueError("Please enter either a pandas series or a string.")
+
+def str_replace_all(string, pattern, replace):
+    if isinstance(string, _pd.Series):
+        _pattern = _compile(pattern)
+        def _pd_rep(x):
+            return _replace(x, pattern, _pattern, replace, 0)
+        return string.apply(_pd_rep)
+    elif isinstance(string, str):
+        _pattern = _compile(pattern)
+        string = _replace(string, pattern, _pattern, replace, 0)
         return string
     else:
         raise ValueError("Please enter either a pandas series or a string.")
--- tests/test_str_replace.py
+++ tests/test_str_replace.py
@@ -1,23 +1,39 @@
 import pytest
 import pandas as pd
 from pandas._testing import assert_frame_equal
-from stringpy.str_replace import str_replace
+from stringpy.str_replace import *
 
 
 def test_str_replace_str():
     string = "The cat sat in the hat."
     pattern = r'at'
-    final = str_replace(string, r'at', "")
     assert str_replace(string, r'at', "") == "The c sat in the hat."
     
 def test_str_replace_df():
     df = pd.DataFrame({
-        'A': ["Cat", "Fat", "Foot"]
+        'A': ["Cat hat", "Fat cat", "Foot hoot"]
     })
     df_test = pd.DataFrame({
-        'A': ["Cat", "Fat", "Foot"],
-        'B': ["C", "F", "Foot"]
+        'A': ["Cat hat", "Fat cat", "Foot hoot"],
+        'B': ["C hat", "F cat", "Foot hoot"]
     })
     pattern = r'at'
     df['B'] = str_replace(df['A'], r'at', "")
+    assert assert_frame_equal(df, df_test) is None
+
+def test_str_replace_all_str():
+    string = "The cat sat in the hat."
+    pattern = r'at'
+    assert str_replace_all(string, r'at', "") == "The c s in the h."
+    
+def test_str_replace_all_df():
+    df = pd.DataFrame({
+        'A': ["Cat hat", "Fat cat", "Foot hoot"]
+    })
+    df_test = pd.DataFrame({
+        'A': ["Cat hat", "Fat cat", "Foot hoot"],
+        'B': ["C h", "F c", "Foot hoot"]
+    })
+    pattern = r'at'
+    df['B'] = str_replace_all(df['A'], r'at', "")
     assert assert_frame_equal(df, df_test) is None
