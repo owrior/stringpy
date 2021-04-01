38e8419afbd6d6157a90aba2cdd0c1468cd9d232
Georgia Tickle <georgiatickle@georgias-air.lan>
2021-04-01 18:08:29 +0100

    Functionalised and added pandas function.

---
+++ src/stringpy/__init__.py
@@ -0,0 +1,1 @@
+from .str_replace import str_replace
--- src/stringpy/str_replace.py
+++ src/stringpy/str_replace.py
@@ -1,25 +1,34 @@
 import pandas as _pd
 import re as _re
 
+def _compile(pattern):
+    if isinstance(pattern, dict):
+        pattern = dict((_re.escape(k), v) for k, v in pattern.items()) 
+        _pattern = _re.compile("|".join(pattern.keys()))
+    elif isinstance(pattern, str):
+        _pattern = _re.compile(pattern)
+    else:
+        raise ValueError("Please enter either a string or dict for pattern.")
+    return _pattern
+
+def _replace(string, _pattern, replace, count):
+    if isinstance(pattern, dict):
+        string = _pattern.sub(lambda m: _rep[_re.escape(m.group(0))], string, count)
+    elif isinstance(pattern, str):
+        string = _pattern.sub(replace, string, count)
+    else:
+        raise ValueError("Please enter either a string or dict for pattern.")
+    return string
 
 def str_replace(string, pattern, replace):
-    if isinstance(pattern, dict):
-        pattern = dict((_re.escape(k), v) for k, v in pattern.items()) 
-        pattern = _re.compile("|".join(pattern.keys()))
-        string = pattern.sub(lambda m: _rep[_re.escape(m.group(0))], string, 1)
-    elif isinstance(pattern, str):
-        string = _re.sub(pattern, replace, string, 1)
+    if isinstance(string, _pd.Series):
+        _pattern = _compile(pattern)
+        def _pd_rep(x):
+            return _replace(x, _pattern, replace, 1)
+        return string.apply(_pd_rep)
+    elif isinstance(string, str):
+        _pattern = _compile(pattern)
+        string = _replace(string, _pattern, replace, 1)
+        return string
     else:
-        raise ValueError("Please enter either a string or dict for pattern.")
-    return string
-
-def str_replace_all(string, pattern, replace):
-    if isinstance(pattern, dict):
-        pattern = dict((_re.escape(k), v) for k, v in pattern.items()) 
-        pattern = _re.compile("|".join(pattern.keys()))
-        string = pattern.sub(lambda m: rep[_re.escape(m.group(0))], string)
-    elif isinstance(pattern, str):
-        string = _re.sub(pattern, replace, string)
-    else:
-        raise ValueError("Please enter either a string or dict for pattern.")
-    return string
+        raise ValueError("Please enter either a pandas series or a string.")
